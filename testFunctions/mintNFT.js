c6728b9c025698fb8ab0ee40fef0d97d7ca2ca20
Матвей <120614670+ArtYu12@users.noreply.github.com>
2024-10-20 00:05:53 +0500

    Delete testFunctions directory
    delete folder

--- testFunctions/mintNFT.js
+++
@@ -1,8 +0,0 @@
-const {mint} = require("../helpers/mintSbt");
-const main = async () => {
-    const minted = await mint(1, "UQBrKi-UxyCvspTDZaLWrthLmiovK326gSLSKo2vyvzLr-w9")
-
-    console.log(minted)
-}
-
-main()
